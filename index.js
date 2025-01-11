762881a26a1df4cad14dd65a572d26c74ab182e9
Airline Plan <hhimanshu030@gmail.com>
2025-01-11 11:30:12 +0530

    backend own build

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ app.use("/", userRoutes);
 app.use(express.static("dist"));
 
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 // app.use(express.static(path.join(__dirname, "../Airlineplan/dist")));
 
 // // Catch-all route to serve the index.html from the frontend dist folder
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../Airlineplan/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../Airlineplan/dist", "index.html"));
+// });
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`server started on ${PORT}`);
