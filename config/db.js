e79c91750ed666876ae44cec1e8edd0357ad8ea0
Himanshu-AIT <admin@airlineplan.com>
2025-03-21 05:32:06 +0530

    db

--- config/db.js
+++ config/db.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 // new developer - Himanshu link 
 // const DB = "mongodb+srv://crazyphoton150hs:C00!buddy@cluster0.4mq6pjf.mongodb.net/?retryWrites=true&w=majority";
-// const DB = "mongodb+srv://hhimanshu030:C00!buddy@cluster0.qondpde.mongodb.net/?retryWrites=true&w=majority";
-// const DB = "mongodb://https://airlineplan.com/airlines"
+const DB = "mongodb+srv://hhimanshu030:C00!buddy@cluster0.qondpde.mongodb.net/?retryWrites=true&w=majority";
+// const DB = "mongodb://http://localhost:3000/airlines"
 
 // const DB = "mongodb://127.0.0.1/airlines";
 
 // Client - Neelandri link
-const DB = "mongodb+srv://neeladrinathsarangi:kBhaZHXuGOIUgt9y@cluster0.n0cx0yj.mongodb.net/?retryWrites=true&w=majority"
+// const DB = "mongodb+srv://neeladrinathsarangi:kBhaZHXuGOIUgt9y@cluster0.n0cx0yj.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose
   .connect(DB, {})
